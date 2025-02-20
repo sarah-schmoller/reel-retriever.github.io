a823c480709c78b20c243722a0bd6268ad72b208
Sarah Schmoller <sarah.schmoller@gmail.com>
2025-02-20 15:38:44 -0600

    Added main page file

---
+++ src/components/HomePage.js
@@ -0,0 +1,18 @@
+
+
+import React, { useEffect, useRef, useState } from 'react';
+import '../styles/homePageStyles.css';
+
+function HomePage() {
+
+
+  // HTML FORMATTING
+  return (
+    <html lang="en">
+    </html>
+
+  );
+}
+
+export default HomePage;
+
