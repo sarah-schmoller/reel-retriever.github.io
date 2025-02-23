b1be61ee81502a23d225f89dad9bfbbb391e97e4
Sarah Schmoller <sarah.schmoller@gmail.com>
2025-02-23 11:48:32 -0600

    Added header html

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -9,6 +9,27 @@ function HomePage() {
   // HTML FORMATTING
   return (
     <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <title>Reel Retriever</title>
+        <link rel="stylesheet" href="styles.css" />
+      </head>
+
+
+      {/* DESKTOP BODY */}
+      <div id="body">
+
+
+        {/* HEADER */}
+        <header class="header"></header>
+
+        <div className="titleBar">
+          <img src="./title-bar.png" alt="Reel Retriever Title Bar" />
+          <div className="titleBarText">
+            Bringing you overlooked videos from across YouTube’s history.
+          </div>
+        </div>
+      </div>
     </html>
 
   );
