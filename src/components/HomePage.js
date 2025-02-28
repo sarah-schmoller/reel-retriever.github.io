a7e165db88f196750751fa95881c5c0628db16e5
Sarah Schmoller <sarah.schmoller@gmail.com>
2025-02-28 16:50:22 -0600

    Added left column contents in html

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -45,10 +45,49 @@ function HomePage() {
             {/* LEFT COLUMN */}
             <div className="leftColumn">
               <img
-                src="./left-sidebar-background.png"
+                src="./leftSidebarBackground.png"
                 alt="Left column background"
                 className="leftColumnBackground"
               />
+              <div className="leftColumnContent">
+                <div className="leftColumnTile">
+                  <img src="./leftWidget.png" alt="Left Widget Bottom" />
+                  <div className="overlayText" id="overlayTextUpperWidgets">
+                    <span className="highlightedText">
+                      219,482 {" "}
+                    </span> 
+                      videos currently indexed
+                  </div>
+                </div>
+                <div className="leftColumnTile">
+                  <img src="./leftWidget.png" alt="Left Widget Bottom" />
+                  <div className="overlayText" id="overlayTextUpperWidgets">
+                    Oldest upload posted in {" "}
+                    <span className="highlightedText">
+                      Jan 2007 
+                    </span>
+                  </div>
+                </div>
+                <div className="leftColumnTile">
+                  <img src="./leftWidget.png" alt="Left Widget Bottom" />
+                  <div className="overlayText" id="overlayTextUpperWidgets">
+                    <span className="highlightedText">
+                      1,482 {" "}
+                    </span>
+                    videos rediscovered today
+                  </div>
+                </div>
+                <div className="leftColumnTile">
+                  <img src="./leftWidgetBottom.png" alt="Left Widget Bottom" />
+                  <div className="overlayText" id="overlayTextLowerWidget">
+                    <span className="highlightedText">
+                      Can't find what you're looking for?
+                    </span> 
+                    <br/>
+                      Some low-view YouTube uploads do not allow embedding, and cannot be displayed directly on Reel Retriever. If you'd like to explore more of this kind of content, select "allow non-embeddable" in the video options, and we will direct you to links for viewing on YouTube.
+                  </div>
+                </div>
+              </div>
             </div>
 
             
